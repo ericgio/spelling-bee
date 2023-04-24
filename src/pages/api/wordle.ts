193bbefbf6c6af38d46a8d5babf3dfc61f411900
Eric Giovanola <ericgio@gmail.com>
2023-04-24 01:41:28 -0700

    Check for local solution in API

--- src/pages/api/wordle.ts
+++ src/pages/api/wordle.ts
@@ -1,13 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import solutions from '../../data/wordle-solutions.json';
+
 const API_URL = 'https://www.nytimes.com/svc/wordle/v2';
 
 async function getSolution(req: NextApiRequest, res: NextApiResponse) {
   const { date } = req.query;
-  const response = await fetch(`${API_URL}/${date}.json`);
-  const data = await response.json();
 
-  res.status(200).json(data);
+  let solution = solutions.find((s) => s.print_date === date);
+
+  if (!solution) {
+    const response = await fetch(`${API_URL}/${date}.json`);
+    solution = await response.json();
+  }
+
+  res.status(200).json(solution);
 }
 
 export default getSolution;
