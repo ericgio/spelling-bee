09dfcba1bf1bac9de7a8b4b6dba64da161712baf
Eric Giovanola <ericgio@gmail.com>
2023-05-04 23:17:23 -0700

    Update input styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const $Input = styled.input`
+  background-clip: padding-box;
   background-color: ${({ theme }) => theme.colors.white};
   border: 2px solid ${({ theme }) => theme.colors.borderColor};
   border-radius: 0.5rem;
@@ -8,8 +9,10 @@ const $Input = styled.input`
   color: ${({ theme }) => theme.colors.black};
   display: block;
   font-size: 1rem;
+  height: calc(2.75rem + 2px);
   line-height: 1.5;
   padding: 0.5rem 0.75rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   width: 100%;
 
   &:focus {
